0c762b452b960b365dac6aa84294f71c2388ade1
XPM56780 <xp.marechal@free.fr>
2024-12-12 11:18:23 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("hello wilders"));
+console.log(chalk.red("hello wilders"));
+console.log(chalk.green("hello wilders"));
+console.log(chalk.yellow("hello wilders"));
